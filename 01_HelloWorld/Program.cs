46fd433cdf8be50b93402c15d0e450a524edac3c
Antony Torres Reyes <antonyetr@outlook.com>
2025-05-20 13:26:51 -0600

    Finished Part 3: Hello World, Your First Program

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, C#'s players guide 1!");
+
+Console.WriteLine("Hello, C#'s players guide 1!");
+Console.WriteLine("Hello, C#'s players guide 2!");
+Console.WriteLine("Hello, C#'s players guide 3!");
+Console.WriteLine("Hello, C#'s players guide 4!");
+Console.WriteLine("Hello, C#'s players guide 5!");
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got the bread.");
